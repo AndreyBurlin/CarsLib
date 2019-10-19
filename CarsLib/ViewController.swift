947ca24cec3d968cfac0a43c81f8165ffa09cf8e
Andrey Burlin <a.burlin@mail.ru>
2019-10-19 11:57:36 +0300

    Initial Commit

---
+++ CarsLib/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarsLib
+//
+//  Created by Андрей Бурлин on 19/10/2019.
+//  Copyright © 2019 Андрей Бурлин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
